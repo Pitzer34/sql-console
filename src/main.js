590b3ca078e3511b5102fd0608f1a804eb44a73b
鍾弼合 <bh.zhong@yfy.com>
2025-01-24 17:29:15 +0800

    <feat> 調整import & use

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router/index';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import { definePreset } from '@primevue/themes';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: definePreset(Aura, {
+      semantic: {
+        primary: {
+          50: '{gray.50}',
+          100: '{gray.100}',
+          200: '{gray.200}',
+          300: '{gray.300}',
+          400: '{gray.400}',
+          500: '{gray.500}',
+          600: '{gray.600}',
+          700: '{gray.700}',
+          800: '{gray.800}',
+          900: '{gray.900}',
+          950: '{gray.950}',
+        },
+      },
+    }),
+    options: {
+      prefix: 'p',
+      darkModeSelector: '.dark',
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind-base, primevue, tailwind-utilities',
+      },
+    },
+  },
+});
+
+app.mount('#app');
