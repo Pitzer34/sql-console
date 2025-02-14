eae914a626ce1029945dd251458bb22fd729c1c0
鍾弼合 <bh.zhong@yfy.com>
2025-02-14 16:10:06 +0800

    <feat>主畫面 切版 & 元件布置

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <script setup>
 import { ref } from 'vue';
-import { RouterView } from 'vue-router';
+import { RouterView, RouterLink } from 'vue-router';
+import Sidebar from './components/Sidebar.vue';
+import { Tabs, TabList, Tab, TabPanels } from 'primevue';
 
-const title = ref('SQL-Library');
+const title = ref('SQL-Maker');
 
+const linkItems = ref([
+  { routeNm: 'Console', label: 'Console', icon: '' },
+  { routeNm: 'SQLite', label: 'SQLite', icon: '' }
+]);
 
 </script>
 
 <template>
-  <main class="mx-auto max-w-7xl h-fvh">
-    <h1 class="text-center text-3xl font-bold my-4">{{ title }}</h1>
-    <RouterView />
+  <main class="mx-auto max-w-7xl h-full">
+    <div class="flex flex-col h-full">
+      <h1 class="text-center text-3xl font-bold py-4">{{ title }}</h1>
+      <div class="flex-1 grid grid-cols-5 gap-2 mx-10">
+        <div class="col-span-1">
+          <Sidebar></Sidebar>
+        </div>
+        <div class="col-span-4">
+          <Tabs value="Console" class="h-full">
+            <TabList>
+              <RouterLink v-for="tab in linkItems" :key="tab.routeNm" :to="{ name: tab.routeNm }">
+                <Tab :value="tab.routeNm">
+                  {{ tab.label }}
+                </Tab>
+              </RouterLink>
+            </TabList>
+            <TabPanels class="p-1 h-full">
+              <RouterView />
+            </TabPanels>
+          </Tabs>
+        </div>
+      </div>
+      <footer class="text-center py-4">
+        {{ "© 2025 Pitzer34. All rights reserved. " }}
+      </footer>
+    </div>
   </main>
 </template>
 
