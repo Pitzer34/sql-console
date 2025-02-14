7edf45c76976da075a07ba71f9fd3efb86e16a79
鍾弼合 <bh.zhong@yfy.com>
2025-02-14 16:13:31 +0800

    <feat>調整路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Sidebar from '../components/Sidebar.vue';
 
 const routes = [
   {
     path: '/',
+    redirect: { name: 'Console' },
+  },
+  {
+    path: '/console',
     name: 'Console',
     component: () => import('../pages/Console.vue'),
+  },
+  {
+    path: '/sqlite',
+    name: 'SQLite',
+    component: () => import('../pages/SQLite.vue'),
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory('SqlLibrary'),
+  history: createWebHistory('/SQLMaker'),
   routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      top: 0,
+      behavior: 'smooth',
+    };
+  },
 });
 
 export default router;
