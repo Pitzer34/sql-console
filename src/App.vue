848214840368f012554a7ce043d854daab2a46c9
Pitzer34 <leo34.pitzer@gmail.com>
2025-02-20 16:50:37 +0800

    <fix>簡化App.vue結構，移除不必要的元件和變數

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,10 @@
 <script setup>
-import { ref } from 'vue';
-import { RouterView, RouterLink } from 'vue-router';
-import Sidebar from './components/Sidebar.vue';
-import { Tabs, TabList, Tab, TabPanels } from 'primevue';
-
-const title = ref('SQL-Maker');
-
-const linkItems = ref([
-  { routeNm: 'Console', label: 'Console', icon: '' },
-  { routeNm: 'SQLite', label: 'SQLite', icon: '' }
-]);
+import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
-  <main class="mx-auto max-w-7xl h-full">
-    <div class="flex flex-col h-full">
-      <h1 class="text-center text-3xl font-bold py-4">{{ title }}</h1>
-      <div class="flex-1 grid grid-cols-5 gap-2 mx-10">
-        <div class="col-span-1">
-          <Sidebar></Sidebar>
-        </div>
-        <div class="col-span-4">
-          <Tabs value="Console" class="h-full">
-            <TabList>
-              <RouterLink v-for="tab in linkItems" :key="tab.routeNm" :to="{ name: tab.routeNm }">
-                <Tab :value="tab.routeNm">
-                  {{ tab.label }}
-                </Tab>
-              </RouterLink>
-            </TabList>
-            <TabPanels class="p-1 h-full">
-              <RouterView />
-            </TabPanels>
-          </Tabs>
-        </div>
-      </div>
-      <footer class="text-center py-4">
-        {{ "© 2025 Pitzer34. All rights reserved. " }}
-      </footer>
-    </div>
-  </main>
+  <RouterView />
 </template>
 
 <style scoped></style>
