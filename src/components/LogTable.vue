fc1f07e2edfa5bcef6731455318ec4702b2a01f0
Pitzer34 <leo34.pitzer@gmail.com>
2025-02-20 16:51:21 +0800

    <feat>移除InputTable.vue元件，新增LogTable.vue以改善數據顯示

---
+++ src/components/LogTable.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const prop = defineProps({
+  data: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+
+
+</script>
+
+<template>
+  <table class="border border-black">
+    <thead>
+      <tr>
+        <th v-for="col in data.columns" :key="col" class="border border-black">
+          {{ col }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="value in data.values" :key="value">
+        <td v-for="item in value" :key="item" class="border border-black">
+          {{ item }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
