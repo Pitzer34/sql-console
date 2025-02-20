4ee5b00cf2b15485f2e621d12d83a494656fd967
Pitzer34 <leo34.pitzer@gmail.com>
2025-02-20 16:49:52 +0800

    <fix>更新Vite配置的基底路徑

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/SQLMaker',
+  base: '/sql-console',
   build: {
     target: 'esnext',
   },
