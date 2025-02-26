45c87ac51ccbc2b9820cbdf111ec8e59eae9988d
Pitzer34 <leo34.pitzer@gmail.com>
2025-02-26 17:30:04 +0800

    feat: 重構左側邊欄，移除公共狀態管理，改用SQL狀態管理顯示資料表列表

--- src/layouts/LeftSidebar.vue
+++ src/layouts/LeftSidebar.vue
@@ -1,33 +1,16 @@
 <script setup>
-import { ref, computed } from 'vue';
-import { usePublicStore } from '../store/publicStore.js';
-import { storeToRefs } from 'pinia';
-import db from '../composables/sql.js';
+import { useSqlStore } from '../store/sqlStore.js';
+
 import PanelMenu from 'primevue/panelmenu';
 
+const sqlStore = useSqlStore();
 
-const publicStore = usePublicStore();
-const { sqlTableLists } = storeToRefs(publicStore);
-
-const listItems = computed(() => {
-  if (!sqlTableLists.value) return;
-  return sqlTableLists.value.map((table) => {
-    const fieldCode = db.exec(`PRAGMA table_info(${table});`)[0].values.map((field) => {
-      return { label: field[1] }
-    });
-    console.log(fieldCode)
-    return {
-      label: table,
-      items: fieldCode,
-    }
-  });
-});
 </script>
 
 <template>
   <div class="flex flex-col border rounded p-2 bg-white h-full overflow-auto">
     <div class="text-xl px-2 py-1.5">{{ "Table List" }}</div>
-    <PanelMenu :model="listItems" multiple pt:panel:class="border-none" />
+    <PanelMenu :model="sqlStore.tableLists" multiple pt:panel:class="" />
   </div>
 </template>
 
