3864c5b45df0b5b9cf027c11e670b07691fbc13a
Pitzer34 <leo34.pitzer@gmail.com>
2025-02-26 17:30:25 +0800

    feat: 重構FieldList元件，整合SQL狀態管理，簡化表格操作邏輯

--- src/pages/FieldList.vue
+++ src/pages/FieldList.vue
@@ -1,74 +1,29 @@
 <script setup>
-import { computed, ref } from 'vue';
-import { storeToRefs } from 'pinia';
-import { usePublicStore } from '../store/publicStore.js';
+import { ref } from 'vue';
+import { useSqlStore } from '../store/sqlStore.js';
 import FieldDialog from '../components/FieldDialog.vue';
-import db from '../composables/sql.js';
-import { DataTable, Column, FloatLabel, InputText, Button } from 'primevue';
+import { DataTable, Column, FloatLabel, InputText, Button, useToast, Toast } from 'primevue';
 import { Icon } from "@iconify/vue";
 
-//* PiniaStore
-const publicStore = usePublicStore();
-const { sqlTableLists, fieldTemplates } = storeToRefs(publicStore);
+//* Store
+const sqlStore = useSqlStore();
 
-const addDialogVisible = ref(false);
-const addNewFieldTemplate = (value) => {
-  fieldTemplates.value.push(value);
-}
-
-const selectedFields = ref();
-const tableNm = ref('employees');
+//* primevue
+const toast = useToast();
 
 const createTable = () => {
-  console.log(selectedFields.value);
-  const fields = selectedFields.value.map(field => {
-    const { fieldCode, type, length, allowNull, default: defaultValue } = field;
-    let fieldStr = `${fieldCode} ${type}`;
-    if (length) fieldStr += ` (${length})`;
-    if (!allowNull) fieldStr += ' NOT NULL';
-    if (defaultValue) fieldStr += ` DEFAULT '${defaultValue}'`;
+  const { isSuccess, message } = sqlStore.createTable();
+  toast.add({ severity: isSuccess ? 'success' : 'error', summary: `${isSuccess ? 'Success' : 'Error'} Message`, detail: message, life: 3000 });
+};
+const addDialogVisible = ref(false);
 
-    return fieldStr;
-  });
-  console.log(fields);
-
-  const primaryKeyFields = selectedFields.value.filter(field => field.primaryKey).map(field => field.fieldCode);
-  console.log(primaryKeyFields)
-
-  let primaryKeyStr = '';
-  if (primaryKeyFields.length > 0) primaryKeyStr = `, PRIMARY KEY (${primaryKeyFields.join(', ')})`;
-
-  const createTableSqlStr = `CREATE TABLE ${tableNm.value} (${fields.join(', ')}${primaryKeyStr});`;
-  console.log(createTableSqlStr)
-
-  db.run(createTableSqlStr);
-  sqlTableLists.value = db.exec("SELECT name FROM sqlite_master WHERE type='table';")[0]?.values.map(item => item[0]);
-  console.log(sqlTableLists.value);
-}
-
-const deleteTemplate = (row) => {
-  const index = fieldTemplates.value.indexOf(row);
-  if (index > -1) {
-    fieldTemplates.value.splice(index, 1);
-  }
-}
-
-// const createTableSqlStr = "CREATE TABLE test(id int, name nvarchar(10))";
-// const addDataSqlStr = "INSERT INTO test VALUES(0, 'HELLO'); \ INSERT INTO test VALUES(1, 'WORLD');";
-
-// db.run(createTableSqlStr);
-// db.run(addDataSqlStr);
-
-// console.log(db.exec("SELECT * FROM test;"));
-// console.log(db.exec("SELECT name FROM sqlite_master WHERE type='table';"))
-// console.log(db.exec("SELECT sql FROM sqlite_master WHERE type='table' AND name='test';"))
 
 
 </script>
 
 <template>
   <div class="flex flex-col h-full">
-    <DataTable :value="fieldTemplates" v-model:selection="selectedFields" dataKey="fieldCode" size="small"
+    <DataTable :value="sqlStore.fields" v-model:selection="sqlStore.selectedFields" dataKey="fieldCode" size="small"
       class="flex-1">
       <template #header>
         <div class="flex items-center justify-between">
@@ -77,22 +32,23 @@ const deleteTemplate = (row) => {
         </div>
       </template>
       <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
-      <Column v-for="col in publicStore.fieldOptions" :key="col.field" :field="col.field" :header="col.header"></Column>
+      <Column v-for="col in sqlStore.fieldOptions" :key="col.field" :field="col.field" :header="col.header"></Column>
       <Column :rowEditor="true" class="min-w-8 w-[8%]">
         <template #body="{ data }">
-          <Icon icon="material-symbols-light:delete-outline" width="24" height="24" @click="deleteTemplate(data)"
+          <Icon icon="material-symbols-light:delete-outline" width="24" height="24" @click="sqlStore.deleteField(data)"
             class="rounded border-red-400 text-red-400 hover:border-2" />
         </template>
       </Column>
     </DataTable>
     <div class="flex gap-4 p-2">
       <FloatLabel variant="on">
-        <InputText id="createTableName" size="small" v-model="tableNm" />
+        <InputText id="createTableName" size="small" v-model="sqlStore.tableName" />
         <label for="createTableName">Table Name</label>
       </FloatLabel>
-      <Button @click="createTable" size="small">Create table</Button>
+      <Toast />
+      <Button @click="createTable()" size="small">Create table</Button>
     </div>
-    <FieldDialog v-model:visible="addDialogVisible" @submit="addNewFieldTemplate"></FieldDialog>
+    <FieldDialog v-model:visible="addDialogVisible" @submit="sqlStore.createField(value)"></FieldDialog>
   </div>
 </template>
 
