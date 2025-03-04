7aedb637cd3b23abb512d0856d4b144261d607d0
Pitzer34 <leo34.pitzer@gmail.com>
2025-03-04 17:12:48 +0800

    feat: 在App元件中新增onMounted鉤子以初始化示例表格

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useSqlStore } from './store/sqlStore.js';
 import { RouterView } from 'vue-router';
 
+const sqlStore = useSqlStore();
+
+onMounted(() => {
+  sqlStore.setExampleTable();
+});
 </script>
 
 <template>
