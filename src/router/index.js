5a9cfaf9525722563b5b85f09b312a4e832f26ea
Pitzer34 <leo34.pitzer@gmail.com>
2025-09-16 13:26:01 +0800

    feat: 更新路由設定，將歷史模式改為使用 hash 模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { useSqlStore } from '../store/sqlStore';
 
 const routes = [
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/sql-console'),
+  history: createWebHashHistory('/sql-console'),
   routes: routes,
   scrollBehavior(to, from, savedPosition) {
     return {
